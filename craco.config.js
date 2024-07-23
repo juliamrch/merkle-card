9c400dad62422c4e9e43af561b35d320d34ced95
cryptoruleseverythingaroundme <zzzz88888888@protonmail.com>
2024-07-23 14:45:09 +0300

    Removes react app

--- craco.config.js
+++
@@ -1,9 +0,0 @@
-const million = require('million/compiler');
-module.exports = {
-  webpack: {
-    plugins: { add: [million.webpack()] }
-  },
-  devServer: {
-    allowedHosts: 'all', // or ['localhost', '.your-domain.com']
-  },
-};
--- src/components/useEnsNameFetch.js
+++
@@ -1,7 +0,0 @@
-import { useEnsName } from "wagmi";
-
-export function useEnsNameFetch(address) {
-  const { data: ensName } = useEnsName({ address });
-  return ensName;
-}
-export default useEnsName;
